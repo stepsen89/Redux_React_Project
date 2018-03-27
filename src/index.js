928a255b3c9a7461200bfcb74568874ee354e8d2
stepsen89 <stephanie.senoner@gmail.com>
2018-03-27 21:42:03 +0200

    update node modules - npm package eslint local updatet

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+const API_KEY = 'AIzaSyBsqij2r9oayq_0wxKsY-AiBXu0cbiAysI'
 
-const API_key = process.env.API_key;
-//create
-const App = function(){
-  return (
-  <div>
-    <SearchBar/>
-  </div>
-  );
+YTSearch({key: API_KEY, term: 'surfing'}, function (data) {
+  console.log(data)
+})
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { videos: [] }
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
