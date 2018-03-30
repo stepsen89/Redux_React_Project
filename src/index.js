9a4b0c16b34b31ae761f693f4ba452a16b7d8646
stepsen89 <stephanie.senoner@gmail.com>
2018-03-30 15:49:38 +0200

    api parsing error, some styling for index.js and components, videodetails

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-
+import VideoDetail from './components/video_detail';
 const API_KEY = config.MY_KEY;
 
 class App extends Component {
-
   constructor (props) {
     super(props)
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'surfing'}, (videos) => {
-      this.setState({ videos });
+    YTSearch({key: API_KEY, term: 'all stars'}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0] 
+      });
     });
   }
 
@@ -22,7 +27,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+        onVideoSelect = {selectedVideo => this.state({selectedVideo}) }
+        videos={this.state.videos} />
       </div>
     )
   }
