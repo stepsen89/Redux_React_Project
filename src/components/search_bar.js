11c0e5ae38fdd085e45e90e1246b76e5d74eca80
stepsen89 <stephanie.senoner@gmail.com>
2018-03-31 14:44:09 +0200

    search term changes detected and debounce

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,11 +9,20 @@ class SearchBar extends Component {
 
   render () {
     return (
-      <div>
-        <input className="searchfield" onChange={event => this.setState({ term: event.target.value })} />
+      <div className="search-bar">
+        <input className="searchfield" 
+          value={this.state.term}
+        onChange={event => this.onInputChange(event.target.value)} />
       </div>
     )
   }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
 }
+
+
 
 export default SearchBar
